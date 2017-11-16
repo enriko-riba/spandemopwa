e2bf8da20e5bd68ce91a038d7cb67aff46429686
Enriko Riba <eriba@span.eu>
2017-11-16 10:43:58 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
-const config = {
-    apiKey: "AIzaSyD0cMNr0yCI-9LCCC7PcbEpOALrSXjCfcg",
-    authDomain: "spandemopwa.firebaseapp.com",
-    databaseURL: "https://spandemopwa.firebaseio.com",
-    projectId: "spandemopwa",
-    storageBucket: "spandemopwa.appspot.com",
-    messagingSenderId: "805463871698"
-  };
+import * as ko from "knockout";
 
 class Main {
-    public test = ko.observable("");
+    private test = ko.observable("radi");
+
+    constructor(){
+      const config = {
+        apiKey: "AIzaSyD0cMNr0yCI-9LCCC7PcbEpOALrSXjCfcg",
+        authDomain: "spandemopwa.firebaseapp.com",
+        databaseURL: "https://spandemopwa.firebaseio.com",
+        projectId: "spandemopwa",
+        storageBucket: "spandemopwa.appspot.com",
+        messagingSenderId: "805463871698"
+      };
+    }
 }
-
-
 export var vm = new Main();
+
+/**
+ * start ko binding.
+ */
+$(document).ready(() => {
+  ko.applyBindings(vm);
+});
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,51 @@
 import * as webpack from 'webpack';
-const p = require("./webpack.plugins");
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+const ProvidePlugin = require("webpack/lib/ProvidePlugin");
+//const ManifestPlugin = require('webpack-manifest-plugin');
+//const ExtractTextPlugin = require('extract-text-webpack-plugin');
+//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const isProd = (require('yargs').argv.env === 'build');
+
+let plugins: Array<webpack.Plugin> = [
+    new CleanWebpackPlugin(['public']), 
+    new CopyWebpackPlugin([{from: 'src/assets/*.png', to:'assets/[name].[ext]' },
+                           //{from: 'src/assets/favicon.ico', to:'assets/[name].[ext]' },
+                           //{from: 'src/pwa_manifest.json', to:'[name].[ext]' },
+                           //{from: 'src/sw.js', to:'[name].[ext]' }
+                        ]),
+    new HtmlWebpackPlugin({ template: './src/index.html',
+                          production: isProd, 
+                          minify: {
+                              collapseWhitespace: false,
+                              collapseInlineTagWhitespace: true,
+                              removeComments: true,
+                              removeRedundantAttributes: true
+                          }}), 
+                          new CommonsChunkPlugin({
+                            names:["main", "vendor", "frb", "runtime"]
+                          }),
+                          new ProvidePlugin({jQuery: 'jquery', $: 'jquery', jquery: 'jquery', ko: 'knockout'}),
+	//new ExtractTextPlugin({filename: "bundle.css", disable: false, allChunks: true}),
+	//new ManifestPlugin(),
+  ];
+
+
+  if(isProd){
+    plugins.push( new UglifyJSPlugin({parallel: true}));
+} else {
+    const hot = require('webpack/lib/HotModuleReplacementPlugin');
+    const nmp = require('webpack/lib/NamedModulesPlugin');
+    plugins.push(
+         new hot(),
+         new nmp()
+    );
+}
+
 const path = require('path');
 
 interface Config extends webpack.Configuration {
@@ -20,7 +66,7 @@ const config : Config = {
     },
 
     // Enable sourcemaps for debugging webpack's output.
-    devtool: p.isProd ? false: "source-map",
+    devtool: isProd ? false: "source-map",
 
     //  will be passed to webpack-dev-server (only if dev server is used)
     devServer: {
@@ -34,9 +80,14 @@ const config : Config = {
     resolve: {
         extensions: [".ts", ".js", ".css", ".sccs", ".json"]
     },
-    plugins: p.plugins,
+    plugins: plugins,
     module: {
         rules: [ 
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            },
             {
                 enforce: "pre",
                 test: /\.js$/,
