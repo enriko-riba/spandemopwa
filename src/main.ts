e66a79892493430ff0cf0dc9c0f06c511264158c
span\mmarkovic <mirna.markovic@span.eu>
2017-11-22 09:55:57 +0100

    avatar image

--- src/main.ts
+++ src/main.ts
@@ -51,16 +51,14 @@ class Main extends Application {
 	* Checks if the current user is signed-in and redirects to HREF_SIGNIN
 	*/
 	public verifyUserAuthentication() {
-		var oldUser = firebase.auth().currentUser;
 		var routeHash = window.location.hash;
-		firebase.auth().onAuthStateChanged((user) => {
-			if (oldUser == null) {
-				FirebaseHelper.isUserSignedIn(routeHash);
-			}
+		var oldUser = firebase.auth().currentUser;
+		firebase.auth().onAuthStateChanged((user) => {			
 			if (user) {
+				if(oldUser ==null) FirebaseHelper.isUserSignedIn(routeHash);
 				this.userIsSignedIn(true);
 				this.currentUser(new UserInfo(user.displayName, user.email, user.emailVerified, user.photoURL, user.uid));
-
+				
 				console.log("sign in");
 				console.log('current user:', user.email);
 			} else {
@@ -70,10 +68,10 @@ class Main extends Application {
 				FirebaseHelper.isUserSignedIn();
 			}
 		},
-			(error) => {
-				console.log(error);
-			});
-
+		(error) => {
+			console.log(error);
+		});
+		
 	}
 
 	public signOut() {
@@ -102,8 +100,11 @@ $(document).ready(() => {
 	// material commponent - tab bar
 	const tabBar = new MDCTabBar(document.querySelector('#toolbar-tab-bar'));
 
-	//USAGE:
+	// let menu = new mdc.menu.MDCSimpleMenu(document.querySelector('.mdc-simple-menu'));
+	// // Add event listener to some button to toggle the menu on and off.
+	// document.querySelector('.tab-bar').addEventListener('click', () => menu.open = !menu.open);
 
+	//swipe USAGE :
 	var el = document.getElementById('swipezone');
 	SwipeDetect.swipedetect(el, function (swipedir) {
 		// swipedir contains either "none", "left", "right", "top", or "down"
