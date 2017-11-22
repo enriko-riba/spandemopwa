74926918513a8a2d81a7cb43759441f688452f7a
span\mmarkovic <mirna.markovic@span.eu>
2017-11-22 12:21:14 +0100

    added hammer js for swipe

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 import * as ko from "knockout";
 import * as $ from "jquery";
-import { ServiceWorkerHelper, FirebaseHelper, UserInfo, SwipeDetect, HREF_SIGNIN } from "./helper";
+import { ServiceWorkerHelper, FirebaseHelper, UserInfo, HREF_SIGNIN } from "./helper";
 import { RouteHelper } from "./routes/routeHelper";
 import { Application } from "./SpaApplication";
 
 //  firebase
 import * as firebase from "firebase/app";
 import "firebase/firestore";
-
 import * as mdc from 'material-components-web';
 import { MDCTabBar, MDCTabBarFoundation } from '@material/tabs';
 
+const Hammer = require("hammerjs");
 // css 
 require('./css/site.scss');
 
 class Main extends Application {
-	public routeHelper : RouteHelper;
+	public routeHelper: RouteHelper;
 	public userIsSignedIn = ko.observable<boolean>(false);
 	private currentUser = ko.observable<UserInfo>(new UserInfo);
 
@@ -24,13 +24,13 @@ class Main extends Application {
 
 		ServiceWorkerHelper.registerServiceWorker('sw.js');
 		FirebaseHelper.initFirebase();
-		
+
 		this.IsDebugToConsoleEnabled(true);
 		this.routeHelper = new RouteHelper(this);
 		this.routeHelper.initRouting();
 
 		var requestedHref = window.location.hash;
-        if(requestedHref == HREF_SIGNIN) requestedHref = '#/home';
+		if (requestedHref == HREF_SIGNIN) requestedHref = '#/home';
 		this.trackAuth(requestedHref);
 	}
 
@@ -44,10 +44,10 @@ class Main extends Application {
 	 */
 	private trackAuth(routeHash) {
 		var oldUser = firebase.auth().currentUser;
-		firebase.auth().onAuthStateChanged((user) => {			
+		firebase.auth().onAuthStateChanged((user) => {
 			if (user) {
 				this.userIsSignedIn(true);
-				this.currentUser(new UserInfo(user.displayName, user.email, user.emailVerified, user.photoURL, user.uid));				
+				this.currentUser(new UserInfo(user.displayName, user.email, user.emailVerified, user.photoURL, user.uid));
 				console.log('current user:', user.email);
 				window.location.href = routeHash;
 			} else {
@@ -56,10 +56,10 @@ class Main extends Application {
 				window.location.href = HREF_SIGNIN;
 			}
 		},
-		(error) => {
-			console.log(error);
-		});		
-    }
+			(error) => {
+				console.log(error);
+			});
+	}
 }
 
 export var vm = new Main();
@@ -81,7 +81,7 @@ $(document).ready(() => {
 		document.querySelector('.second-mdc-list').addEventListener('click', () => {
 			document.querySelector('.mdc-temporary-drawer').classList.remove('mdc-temporary-drawer--open');
 		}, false);
-		
+
 	});
 
 	// material commponent - tab bar
@@ -91,13 +91,15 @@ $(document).ready(() => {
 	// Add event listener to some button to toggle the menu on and off.
 	document.querySelector('.tab-bar').addEventListener('click', () => menu.open = !menu.open);
 
-	//swipe USAGE :
-	// var el = document.getElementById('swipezone');
-	// SwipeDetect.swipedetect(el, function (swipedir) {
-	// 	// swipedir contains either "none", "left", "right", "top", or "down"
-	// 	if (swipedir === "right") {
-	// 		drawer.open = true;
-	// 	}
-	// });
+	let swipezone = document.querySelector('#swipezone');
+	var manager = new Hammer.Manager(swipezone);
+	var swipe = new Hammer.Swipe();
+	manager.add(swipe);
+	manager.on('swipe', (e) => {
+		var direction = e.offsetDirection;
+		if (direction === 4) {
+			drawer.open = true;
+		}
 
+	});
 });
