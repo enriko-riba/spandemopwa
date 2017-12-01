846c508a768261a80ec36441a136f05eec4a6249
Enriko Riba <eriba@span.eu>
2017-12-01 12:18:21 +0100

    streaming

--- src/routes/image-stream.ts
+++ src/routes/image-stream.ts
@@ -8,7 +8,35 @@ import { FirebaseHelper } from "../helper";
     template: require('./image-stream.html')
 })
 export class ImageStreamVM {
-    constructor() {
+    private user;
+    private firestoreRef: firebase.firestore.CollectionReference;
+    private images = ko.observableArray<ImageDoc>();
 
+    constructor() {
+        FirebaseHelper.checkUserAndRedirectToSignin();
+        this.user = firebase.auth().currentUser;
+        if(this.user){
+            this.setupImageStreaming();            
+        }
     }
+
+    private setupImageStreaming(){
+        this.firestoreRef = firebase.firestore().collection('images');
+        this.firestoreRef.orderBy('created', 'desc').limit(5)
+            .onSnapshot( collection => {
+                var tmpArray:Array<ImageDoc> = [];
+                collection.docs.forEach((value, idx, array) => {
+                    var data = value.data() as ImageDoc;
+                    
+                    tmpArray.push(data);
+                })
+                this.images(tmpArray);
+        });
+    }
+}
+
+interface ImageDoc {
+    created: Date,
+    downloadURL: string,
+    imgMetadata: any
 }
