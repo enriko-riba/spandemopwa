d52940b47b80070f270fa24e14e1d7f488541fd2
span\mmarkovic <mirna.markovic@span.eu>
2017-12-05 16:15:37 +0100

    recording audio data

--- src/routes/speech.ts
+++ src/routes/speech.ts
@@ -6,6 +6,7 @@ import { ViewModelBase, RouteNavigationData } from "../SpaApplication";
 import { UserMediaHelper, MediaDeviceInfo1 } from "../UserMediaHelper";
 import { firestore } from "firebase/app";
 
+declare var MediaRecorder: any;
 @Component({
     name: 'speech',
     template: require('./speech.html')
@@ -18,7 +19,11 @@ export class SpeechToText extends ViewModelBase {
     private storage = firebase.storage();
     private firestore = firebase.firestore();
 
-    private streamData;
+    private streamData = [];
+    private sourceAudio = ko.observable<any>();
+
+    private mediaRecording; // = new MediaRecorder;
+
     constructor() {
         super();
         FirebaseHelper.checkUserAndRedirectToSignin();
@@ -37,15 +42,42 @@ export class SpeechToText extends ViewModelBase {
 
     private streamAudio = (): Promise<void> => {
         return this.umh.setAudioDevice(this.umh.audioDevices[0])
-            .then((s: MediaStream) => {
-                this.isStreaming(true);
-                console.log(s);
-            });
+            .then(this.handleAudioStreamSuccess);
     }
+
+    private handleAudioStreamSuccess = (stream) => {
+        this.isStreaming(true);
+        
+        this.mediaRecording = new MediaRecorder(stream);
+        this.mediaRecording.start();
+        console.log( this.mediaRecording.state);
+        console.log("recorder started");
+
+        this.streamData = [];
+        
+        this.mediaRecording.ondataavailable = (e)=> {
+            console.log(e);
+            this.streamData.push(e.data);
+        }
+
+        this.mediaRecording.onstop = (e) => {
+            console.log("recorder stopped");
+
+            var blob = new Blob(this.streamData, { 'type' : 'audio/ogg; codecs=opus' });
+            this.streamData = [];
+            var audioURL = window.URL.createObjectURL(blob);
+            this.sourceAudio(audioURL);
+
+          }
+    }
+
 
     private stopStream = () => {
         this.umh.stopStreaming();
         this.isStreaming(false);
+        console.log("recorder started");
+        this.mediaRecording.stop();
+        console.log(this.streamData);
     }
 }
 
